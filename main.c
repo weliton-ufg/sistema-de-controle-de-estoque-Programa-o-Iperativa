1500c3238ce65c92f18ce22236c5d034898614a4
weliton-ufg <weliton.ufg.inf.si@gmail.com>
2016-11-29 00:50:14 -0200

    implementando os menus

--- main.c
+++ main.c
@@ -1,16 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<locale.h>
 #include "fornecedor.c"
 #include "menu.c"
-int main()
-{
-    printf("Hello world!\n");
-    menurPrincipal();
-  // Fornecedor fornecedor;
-  // fornecedor.codigo=1;
-   //scanf("&c",fornecedor.endereco.bairro);
-  // fornecedor.endereco.codigo=2;
- //  printf("%d\n",fornecedor.codigo);
-  // printf("%d\n",fornecedor.endereco.codigo);
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    while(1){
+        char opcao;
+        VOLTARMENURPRINCIPAL:
+        opcao=menurPrincipal();
+        fflush(stdin);
+        switch(opcao){
+            case 'f': case'F':
+                system("cls");
+                opcao=menurFornecedores();
+                fflush(stdin);
+                switch(opcao){
+                    case 'c': case'C':
+                    break;
+                    case 'l': case'L':
+                    break;
+                    case 'b': case'B':
+                    break;
+                    case 'a': case'A':
+                    break;
+                    case 'v': case'V':
+                        system("cls");
+                        goto VOLTARMENURPRINCIPAL;
+                    break;
+                    case 's': case'S':
+                        mensagemFimPrograma();
+                        exit(0);
+                    break;
+                }
+            break;
+            case 'p': case'P':
+            break;
+            case 'i': case'I':
+            break;
+            case 's': case'S':
+                mensagemFimPrograma();
+                exit(0);
+            break;
+        }
+
+    }
     return 0;
 }
--- menu.c
+++ menu.c
@@ -1,8 +1,37 @@
-void menurPrincipal(){
-    printf("\nDigite um comando para prosseguir:");
-    printf("\nF – Gerenciar Fornecedores");
-    printf("\nP – Gerenciar Produtos");
-    printf("\nI – Informações Gerenciais");
-    printf("\nS – Sair");
+char* menurPrincipal(){
+    char opcao;
+    printf("\n###############      MENUR PRINCIPAL      ################");
+    printf("\n#                                                        #");
+    printf("\n#        Digite um comando para prosseguir:              #");
+    printf("\n#                                                        #");
+    printf("\n#        F – Gerenciar Fornecedores                      #");
+    printf("\n#        P – Gerenciar Produtos                          #");
+    printf("\n#        I – Informações Gerenciais                      #");
+    printf("\n#        S – Sair                                        #");
+    printf("\n#                                                        #");
+    printf("\n##########################################################\n");
+   return opcao=getchar();
+}
+char* menurFornecedores(){
+    char opcao;
+    printf("\n#############     GESTÃO DE FORNECEDORES     #############");
+    printf("\n#                                                        #");
+    printf("\n#        Digite um comando para prosseguir:              #");
+    printf("\n#                                                        #");
+    printf("\n#        C – Cadastrar um fornecedor.                    #");
+    printf("\n#        L – Listar todos os fornecedores cadastrados.   #");
+    printf("\n#        B – Buscar fornecedor já cadastrado.            #");
+    printf("\n#        A – Atualizar um fornecedor cadastrado.         #");
+    printf("\n#        V - Voltar ao menur anterior.                   #");
+    printf("\n#        S – Sair                                        #");
+    printf("\n#                                                        #");
+    printf("\n##########################################################\n");
+    return opcao=getchar();
 }
 
+void mensagemFimPrograma(){
+    printf("\n#######   OBRIGADO POR ULTILIZAR NOSSO SISTEMA    ########");
+    printf("\n#######         AGUARDAREMOS SEU RETORNO          ########");
+    printf("\n#######     PROGRAMA FINALIZADO COM SUCESSO       ########");
+    printf("\n##########################################################\n");
+}
